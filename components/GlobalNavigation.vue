e3492c581d73ff29dadd107976cce28d7c4db575
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2021-04-10 11:55:03 +0100

    :lipstick: Create draft of nav with buttons

---
+++ components/GlobalNavigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- TODO: Make navbar collapsible on mobile devices and use the @apply technique to tidy up CSS classes -->
+  <nav
+    class="nav absolute top-0 shadow-sm h-auto w-full flex flex-col md:flex-row justify-center leading-6 md:space-x-4 space-y-1 md:space-y-0 pl-1 py-1 md:py-3 md:px-0"
+  >
+    <a
+      href="#"
+      class="flex-1 bg-green-800 hover:bg-green-700 focus:ring-2 focus:ring-green-300 text-white px-4 py-1 rounded text-center max-w-min"
+    >
+      Home
+    </a>
+    <a
+      href="#"
+      class="flex-1 bg-green-800 hover:bg-green-700 focus:ring-2 focus:ring-green-300 text-white px-4 py-1 rounded text-center max-w-min"
+    >
+      About
+    </a>
+    <a
+      href="#"
+      class="flex-1 bg-green-800 hover:bg-green-700 focus:ring-2 focus:ring-green-300 text-white px-4 py-1 rounded text-center max-w-min"
+    >
+      Projects
+    </a>
+    <a
+      href="#"
+      class="flex-1 bg-green-800 hover:bg-green-700 focus:ring-2 focus:ring-green-300 text-white px-4 py-1 rounded text-center max-w-min"
+    >
+      Contact
+    </a>
+  </nav>
+</template>
