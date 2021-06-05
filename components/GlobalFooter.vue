4eda508b25f947162a0e9b8f9d088e233c64cdb5
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2021-06-05 18:28:13 +0100

    💄 Add GlobalFooter

---
+++ components/GlobalFooter.vue
@@ -0,0 +1,109 @@
+<template>
+  <footer class="text-center flex flex-col items-center">
+    <div class="icon-container">
+      <a href="https://twitter.com/eStivaros" class="footer-icon">
+        <svg
+          class="h-8 w-8 text-green-900"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" />
+          <path
+            d="M22 4.01c-1 .49-1.98.689-3 .99-1.121-1.265-2.783-1.335-4.38-.737S11.977 6.323 12 8v1c-3.245.083-6.135-1.395-8-4 0 0-4.182 7.433 4 11-1.872 1.247-3.739 2.088-6 2 3.308 1.803 6.913 2.423 10.034 1.517 3.58-1.04 6.522-3.723 7.651-7.742a13.84 13.84 0 0 0 .497 -3.753C20.18 7.773 21.692 5.25 22 4.009z"
+          />
+        </svg>
+      </a>
+      <a href="https://blog.stivaros.com" class="footer-icon">
+        <svg
+          class="h-8 w-8 text-green-900"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M12 19l7-7 3 3-7 7-3-3z" />
+          <path d="M18 13l-1.5-7.5L2 2l3.5 14.5L13 18l5-5z" />
+          <path d="M2 2l7.586 7.586" />
+          <circle cx="11" cy="11" r="2" />
+        </svg>
+      </a>
+      <a href="https://www.linkedin.com/in/estivaros/" class="footer-icon">
+        <svg
+          class="h-8 w-8 text-green-900"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path
+            d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+          />
+          <rect x="2" y="9" width="4" height="12" />
+          <circle cx="4" cy="4" r="2" />
+        </svg>
+      </a>
+      <a href="https://github.com/stivaros" class="footer-icon">
+        <svg
+          class="h-8 w-8 text-green-900"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path
+            d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+          />
+        </svg>
+      </a>
+      <a href="https://ntscreative.co.uk" class="footer-icon">
+        <svg
+          class="h-8 w-8 text-green-900"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <rect x="2" y="7" width="20" height="14" rx="2" ry="2" />
+          <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16" />
+        </svg>
+      </a>
+    </div>
+    <p class="mb-4">&copy; Efstathios Stivaros 2021</p>
+  </footer>
+</template>
+
+<script></script>
+
+<style lang="scss">
+.icon-container {
+  @apply flex items-center gap-4;
+}
+
+a.footer-icon {
+  box-shadow: 0px -0.25rem 0px theme('colors.green.300') inset;
+  @apply border-b-2 border-green-300 px-2 pb-1;
+  @apply transition-all duration-200 ease-in;
+
+  svg {
+    @apply mt-4;
+  }
+
+  &:hover {
+    box-shadow: 0px -40px 0px theme('colors.green.100') inset;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,18 @@
   <div class="container">
     <GlobalNavigation />
     <Nuxt />
+    <GlobalFooter />
   </div>
 </template>
 
 <script>
 import GlobalNavigation from '@/components/GlobalNavigation'
+import GlobalFooter from '@/components/GlobalFooter'
 
 export default {
   components: {
     GlobalNavigation,
+    GlobalFooter,
   },
 }
 </script>
