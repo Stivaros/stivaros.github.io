4b7d6b4becedfa3c082601736c526480caee795a
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2021-06-05 12:40:11 +0100

    :iphone: Make navbar collapsible

--- components/GlobalNavigation.vue
+++ components/GlobalNavigation.vue
@@ -1,31 +1,65 @@
 <template>
-  <!-- TODO: Make navbar collapsible on mobile devices and use the @apply technique to tidy up CSS classes -->
-  <nav
-    class="nav absolute top-0 shadow-sm h-auto w-full flex flex-col md:flex-row justify-center leading-6 md:space-x-4 space-y-1 md:space-y-0 pl-1 py-1 md:py-3 md:px-0"
-  >
-    <a
-      href="#"
-      class="flex-1 bg-green-800 hover:bg-green-700 focus:ring-2 focus:ring-green-300 text-white px-4 py-1 rounded text-center max-w-min"
+  <!-- TODO: Use the @apply technique to tidy up CSS classes -->
+  <nav class="nav absolute top-0 shadow-sm h-auto w-full">
+    <button
+      class="inline-block md:hidden w-8 h-8 text-green-700 p-1 m-1 rounded border border-green-700 float-right focus:ring-2 focus:ring-green-300"
+      @click="isOpen = !isOpen"
     >
-      Home
-    </a>
-    <a
-      href="#"
-      class="flex-1 bg-green-800 hover:bg-green-700 focus:ring-2 focus:ring-green-300 text-white px-4 py-1 rounded text-center max-w-min"
+      <svg
+        fill="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          v-if="isOpen"
+          d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"
+        />
+        <path
+          v-if="!isOpen"
+          fill-rule="evenodd"
+          d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </button>
+    <div
+      :class="isOpen ? 'block' : 'hidden'"
+      class="flex flex-col md:flex-row leading-6 md:space-x-4 space-y-1 md:space-y-0 pl-1 py-1 md:py-3 md:px-0 md:block w-1/3 md:w-full"
     >
-      About
-    </a>
-    <a
-      href="#"
-      class="flex-1 bg-green-800 hover:bg-green-700 focus:ring-2 focus:ring-green-300 text-white px-4 py-1 rounded text-center max-w-min"
-    >
-      Projects
-    </a>
-    <a
-      href="#"
-      class="flex-1 bg-green-800 hover:bg-green-700 focus:ring-2 focus:ring-green-300 text-white px-4 py-1 rounded text-center max-w-min"
-    >
-      Contact
-    </a>
+      <a
+        href="#"
+        class="bg-green-800 hover:bg-green-700 focus:ring-2 focus:ring-green-300 text-white px-4 py-1 rounded"
+      >
+        Home
+      </a>
+      <a
+        href="#"
+        class="bg-green-800 hover:bg-green-700 focus:ring-2 focus:ring-green-300 text-white px-4 py-1 rounded"
+      >
+        About
+      </a>
+      <a
+        href="#"
+        class="bg-green-800 hover:bg-green-700 focus:ring-2 focus:ring-green-300 text-white px-4 py-1 rounded"
+      >
+        Projects
+      </a>
+      <a
+        href="#"
+        class="bg-green-800 hover:bg-green-700 focus:ring-2 focus:ring-green-300 text-white px-4 py-1 rounded"
+      >
+        Contact
+      </a>
+    </div>
   </nav>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
+}
+</script>
