70a700c41f05b6316de9cea73d7b113e51f81c95
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2021-09-18 21:33:52 +0100

    🔍️ Update page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'stivaros.github.io',
+    title: 'Efstathios Stivaros | Ruby on Rails Developer from London',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
