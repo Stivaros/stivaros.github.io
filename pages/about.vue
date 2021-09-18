5aaeb7fd3cdd30554aefb59d521070f1195c7bf4
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2021-09-18 21:30:30 +0100

    💬 Add About page

---
+++ pages/about.vue
@@ -0,0 +1,49 @@
+<template>
+  <main class="split-container">
+    <section class="lg:w-1/2 flex items-center">
+      <article class="space-y-8 px-6 text-lg">
+        <h1 class="pt-8">About Me</h1>
+        <p>
+          I'm a full-stack Ruby on Rails developer with experience both in-house
+          (at both technical and non-technical companies) and at a consultancy.
+        </p>
+        <p>
+          I enjoy working in teams, no matter the size, whether that be in a
+          leadership role or as a team member. I have built, mentored and
+          managed teams from scratch.
+        </p>
+        <p>
+          My focus is on writing clean, well-tested code that will delight the
+          business and my fellow developers alike. I follow the DevOps
+          philosophy and enjoy learning all things tech. Accompanying, you will
+          find some of the projects I've worked on, across the stack.
+          Deployment, governance/policy and architecture are difficult to
+          showcase so please get in touch to discuss.
+        </p>
+      </article>
+    </section>
+    <section class="right-panel flex items-center">
+      <article class="space-y-8 py-8 px-6">
+        <p class="h1">My Skills</p>
+        <ul class="text-lg list-disc list-inside">
+          <li>Ruby on Rails 5.2 through 6.1 (with a keen interest in 7)</li>
+          <li>Rails templating with ERB, HAML and Slim</li>
+          <li>Ruby 2.4 onwards</li>
+          <li>Stimulus.js</li>
+          <li>Basic React (including Gatsby)</li>
+          <li>Basic Vue.js 2 (including Nuxt.js; CSR, SSG and SSR)</li>
+          <li>HTML 4.1 onwards</li>
+          <li>CSS 2 onwards</li>
+          <li>Architecture and software patterns for Rails projects</li>
+          <li>Basic cloud engineering</li>
+        </ul>
+      </article>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
